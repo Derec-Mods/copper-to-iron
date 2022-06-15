84d46b86fe3c503ff1967556b7d044a528ed78c6
Derek Chen <derex1987@gmail.com>
2022-06-15 02:31:39 -0500

    NOW IT GIVES YOU IRON BACK HAHWHAWHA

--- src/main/java/net/derex/tutorialmod/block/custom/CopperConverter.java
+++ src/main/java/net/derex/tutorialmod/block/custom/CopperConverter.java
@@ -6,6 +6,8 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
@@ -25,13 +27,14 @@ public class CopperConverter extends Block
         if (world.isClient) return ActionResult.SUCCESS;
         Inventory blockEntity = (Inventory) world.getBlockEntity(pos);
 
-
         if(!player.getStackInHand(hand).isEmpty())
         {
             if(player.getStackInHand(hand).getItem().equals(Items.COPPER_INGOT))
             {
                 int cnt = player.getStackInHand(hand).getCount();
                 player.getStackInHand(hand).setCount(0);
+                player.giveItemStack(new ItemStack(Items.IRON_INGOT));
+//                world.playSound(null, pos, SoundEvents.ENTITY_SPLASH_POTION_BREAK,);
             }
 //            player.getStackInHand(hand).setCount(0);
         }
